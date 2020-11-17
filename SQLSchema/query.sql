8134c27cb519b47d03e22c563ac8f8a885f8eec5
SKumble <skumble27@gmail.com>
2020-11-17 22:53:00 +1100

    created seperate files for e t and l

--- SQLSchema/query.sql
+++ SQLSchema/query.sql
@@ -4,6 +4,18 @@ FROM accommodation
 INNER JOIN council_city ON 
 accommodation.city = council_city.city
 
--- 
-SELECT eateries.eatery_name, eateries.eatery_rating, eateries.city
-FROM eateries WHERE eateries.eatery_rating BETWEEN 4 AND 5
+-- 2. Inner join two tables, eateries with population
+SELECT eateries.eatery_name, eateries.city, accommodation.accommodation_name,
+accommodation.accommodation_rating
+FROM eateries
+INNER JOIN accommodation ON
+eateries.city = accommodation.city 
+
+-- 3. Inner join of eateries with historical hotels
+SELECT eateries.eatery_name, eateries.city, eateries.eatery_rating, historical_hotel.historical_hotel
+FROM eateries
+INNER JOIN historical_hotel ON
+historical_hotel.city = eateries.city 
+
+-- 4. Multiple Joins - Eatery, city, accommodation, hotel, population
+
