b26b8f1ed9fbb819952877be80e0c3c0265f9375
SKumble <skumble27@gmail.com>
2020-11-17 20:28:29 +1100

    final script being consolidated

--- Final_compilation/SQLSchema/query.sql
+++
@@ -1,9 +0,0 @@
--- Obtain the council and city based on accomodations and rating
-SELECT accommodation.city, accommodation.accommodation_name,accommodation.accommodation_rating, council_city.council
-FROM accommodation
-INNER JOIN council_city ON 
-accommodation.city = council_city.city
-
--- 
-SELECT eateries.eatery_name, eateries.eatery_rating, eateries.city
-FROM eateries WHERE eateries.eatery_rating BETWEEN 4 AND 5
--- Sumukh/PostgreSQL/query.sql
+++
@@ -1,4 +0,0 @@
-SELECT councils.city, population.council, population.areasqkm, population.populationdensity
-FROM Councils
-INNER JOIN population on 
-population.council = councils.council
